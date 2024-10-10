36364ed44de51f0be7b8bed5b1951fef12b634ce
tidiong <62657965+tidiong@users.noreply.github.com>
2024-10-10 14:59:43 -0500

    style changes

--- frontend/vite-project/src/App.jsx
+++ frontend/vite-project/src/App.jsx
@@ -20,6 +20,17 @@ function App() {
       <p><strong>Welcome! </strong>I applaud you for choosing to feed your self care. Add your affirmations to the page by clicking affirm.</p>
       <NewNoteForm addNote={addNote} />
       <NoteList notes={notes} />
+      <p className="signature">
+        Made by {' '}
+        <a
+          href="https://www.linkedin.com/in/darimaidiong/"
+          target="_blank"
+          rel="noopener noreferrer"
+          style={{ color: '#4CAF50' }} // Style the link color
+        >
+          Tea idiong
+        </a>
+      </p>
     </div>
   );
 }
